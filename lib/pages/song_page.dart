07d6434eb3ff23395a2255243ef1f644753ed49a
AyushmanKS <ayushmanks1845@gmail.com>
2024-02-20 00:14:01 +0530

    20Feb '24

--- lib/pages/song_page.dart
+++ lib/pages/song_page.dart
@@ -1,13 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sangeet_flutter/components/neu_box.dart';
+import 'package:sangeet_flutter/models/playlist_provider.dart';
 
 class SongPage extends StatelessWidget {
   const SongPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('S O N G   P A G E'),
+    return Consumer<PlaylistProvider>(
+      builder: (context, value, child) => SafeArea(
+        child: Scaffold(
+            backgroundColor: Theme.of(context).colorScheme.background,
+            body: Column(
+              children: [
+                // app bar
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      // back button
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.arrow_back_ios),
+                      ),
+                      // title
+                      const Text(
+                        'Playlist',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 22),
+                      ),
+                      // menu button
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.menu),
+                      ),
+                    ],
+                  ),
+                ),
+
+                // album artwork
+                NeuBox(
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Image.asset("assets/images/adiyeimg.jpg"),
+                      ),
+
+                      // song and artist name and heart icon
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          // song and artist name
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Adiye',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 20),
+                              ),
+                              Text('Dhibu Ninan Thomas and Kapil Kapilan'),
+                            ],
+                          ),
+                          // heart icon: like
+                          Icon(
+                            Icons.favorite,
+                            color: Colors.red,
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                //song duration progress bar
+
+                // playback controls
+              ],
+            )),
       ),
     );
   }
