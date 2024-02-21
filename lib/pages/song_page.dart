6695b0d5aa33d465418a256abf5455716afc9b5f
AyushmanKS <ayushmanks1845@gmail.com>
2024-02-21 20:06:29 +0530

    21Feb song_page: starting to fetch playlist, curr song

--- lib/pages/song_page.dart
+++ lib/pages/song_page.dart
@@ -9,160 +9,167 @@ class SongPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<PlaylistProvider>(
-      builder: (context, value, child) => SafeArea(
-        child: Scaffold(
-            backgroundColor: Theme.of(context).colorScheme.background,
-            body: Column(
-              children: [
-                // app bar
-                Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      // back button
-                      IconButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        icon: const Icon(Icons.arrow_back_ios),
-                      ),
-                      // title
-                      const Text(
-                        'Playlist',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 22),
-                      ),
-                      // menu button
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(Icons.menu),
-                      ),
-                    ],
-                  ),
-                ),
+      builder: (context, value, child) {
+        // get playlist
 
-                // album artwork
-                Padding(
-                  padding: const EdgeInsets.all(55.0),
-                  child: NeuBox(
-                    child: Column(
+        // get current song index
+
+        // returning scaffold ui from here
+        return SafeArea(
+          child: Scaffold(
+              backgroundColor: Theme.of(context).colorScheme.background,
+              body: Column(
+                children: [
+                  // app bar
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(8),
-                          child: Padding(
-                            padding: const EdgeInsets.all(28.0),
-                            child: Image.asset("assets/images/adiyeimg.jpg"),
-                          ),
+                        // back button
+                        IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: const Icon(Icons.arrow_back_ios),
                         ),
-
-                        // song and artist name and heart icon
-                        const Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            // song and artist name
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Adiye',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 20),
-                                ),
-                                Text('Dhibu Ninan Thomas and Kapil Kapilan'),
-                              ],
-                            ),
-                            // heart icon: like
-                            Icon(
-                              Icons.favorite,
-                              color: Colors.red,
-                            )
-                          ],
+                        // title
+                        const Text(
+                          'Playlist',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 22),
+                        ),
+                        // menu button
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.menu),
                         ),
                       ],
                     ),
                   ),
-                ),
 
-                const SizedBox(height: 18),
-
-                //song duration progress bar
-                const Column(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 25),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  // album artwork
+                  Padding(
+                    padding: const EdgeInsets.all(55.0),
+                    child: NeuBox(
+                      child: Column(
                         children: [
-                          Text('0:00'),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(8),
+                            child: Padding(
+                              padding: const EdgeInsets.all(28.0),
+                              child: Image.asset("assets/images/adiyeimg.jpg"),
+                            ),
+                          ),
 
-                          // shuffle
-                          Icon(Icons.shuffle),
-
-                          // repeat
-                          Icon(Icons.repeat),
-
-                          // end time
-                          Text('0:00'),
+                          // song and artist name and heart icon
+                          const Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              // song and artist name
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Adiye',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20),
+                                  ),
+                                  Text('Dhibu Ninan Thomas and Kapil Kapilan'),
+                                ],
+                              ),
+                              // heart icon: like
+                              Icon(
+                                Icons.favorite,
+                                color: Colors.red,
+                              )
+                            ],
+                          ),
                         ],
                       ),
                     ),
-                  ],
-                ),
-                // Slider
-                Padding(
-                  padding: const EdgeInsets.all(14.0),
-                  child: Slider(
-                    min: 0,
-                    max: 100,
-                    value: 50,
-                    activeColor: Colors.orange,
-                    onChanged: (value) {},
                   ),
-                ),
 
-                // playback controls
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 34.0),
-                  child: Row(
+                  const SizedBox(height: 18),
+
+                  //song duration progress bar
+                  const Column(
                     children: [
-                      // skip previous
-                      Expanded(
-                        child: GestureDetector(
-                          onTap: () {},
-                          child: const NeuBox(
-                            child: Icon(Icons.skip_previous),
-                          ),
-                        ),
-                      ),
-                      // play/pause button
-                      const SizedBox(width: 10),
-                      Expanded(
-                        flex: 2,
-                        child: GestureDetector(
-                          onTap: () {},
-                          child: const NeuBox(
-                            child: Icon(Icons.play_arrow),
-                          ),
-                        ),
-                      ),
-                      // skip forward
-                      const SizedBox(width: 10),
-                      Expanded(
-                        child: GestureDetector(
-                          onTap: () {},
-                          child: const NeuBox(
-                            child: Icon(Icons.skip_next),
-                          ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 25),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text('0:00'),
+
+                            // shuffle
+                            Icon(Icons.shuffle),
+
+                            // repeat
+                            Icon(Icons.repeat),
+
+                            // end time
+                            Text('0:00'),
+                          ],
                         ),
                       ),
                     ],
                   ),
-                ),
-              ],
-            )),
-      ),
+                  // Slider
+                  Padding(
+                    padding: const EdgeInsets.all(14.0),
+                    child: Slider(
+                      min: 0,
+                      max: 100,
+                      value: 50,
+                      activeColor: Colors.orange,
+                      onChanged: (value) {},
+                    ),
+                  ),
+
+                  // playback controls
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 34.0),
+                    child: Row(
+                      children: [
+                        // skip previous
+                        Expanded(
+                          child: GestureDetector(
+                            onTap: () {},
+                            child: const NeuBox(
+                              child: Icon(Icons.skip_previous),
+                            ),
+                          ),
+                        ),
+                        // play/pause button
+                        const SizedBox(width: 10),
+                        Expanded(
+                          flex: 2,
+                          child: GestureDetector(
+                            onTap: () {},
+                            child: const NeuBox(
+                              child: Icon(Icons.play_arrow),
+                            ),
+                          ),
+                        ),
+                        // skip forward
+                        const SizedBox(width: 10),
+                        Expanded(
+                          child: GestureDetector(
+                            onTap: () {},
+                            child: const NeuBox(
+                              child: Icon(Icons.skip_next),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              )),
+        );
+      },
     );
   }
 }
